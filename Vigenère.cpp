e9c71c40691915015c199a64767baf08db680f5d
samuelishida <samuel.toyoshi@gmail.com>
2014-06-29 18:20:23 -0300

    Update Vigenère.cpp

--- Vigenère.cpp
+++ Vigenère.cpp
@@ -11,15 +11,15 @@ int main(int agrc, char *argv[])
 	num += argv[1];
 	chave += argv[2];
 
-    reverse(num.begin(),num.end());//só pq ele faz do contrario
+    reverse(num.begin(),num.end());
     reverse(chave.begin(),chave.end());
 
     for(int i=0, j=0; i < num.length(); i++, j++)
         cifra.push_back(48 + (  (num[i]-48)  +   (chave[j%chave.length()]-48)   )%10);
-        //os -48 é pq os números são na vdd caracteres, sendp 48 o 0 e 57 o 9
+        
 
-    reverse(cifra.begin(),cifra.end());//pra imprimir na ordem certa
-
+    reverse(cifra.begin(),cifra.end());
+    
     cout << cifra << endl;
 
     return 0;
